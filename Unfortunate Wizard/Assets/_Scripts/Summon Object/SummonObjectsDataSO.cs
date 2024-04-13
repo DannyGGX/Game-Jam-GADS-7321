b7f8d2dadc6742ef0da61db4cb68b8d0d8c9fe97
DannyGGX <danielglanz111@gmail.com>
2024-04-13 22:30:08 +0200

    Created Summon Object Data classes

---
+++ Unfortunate Wizard/Assets/_Scripts/Summon Object/SummonObjectsDataSO.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+[CreateAssetMenu(fileName = "Summon Objects Data", menuName = "Scriptable Object/Summon Objects Data")]
+public class SummonObjectsDataSO : ScriptableObject
+{
+    [SerializeField] private List<SummonObjectData> summonObjectDataList = new();
+
+    private void SetIds()
+    {
+        for (int i = 0; i < summonObjectDataList.Count; i++)
+        {
+            summonObjectDataList[i].id = i;
+        }
+    }
+
+    private void OnValidate()
+    {
+        SetIds();
+    }
+    
+    public SummonObjectData GetSummonObjectData(int id)
+    {
+        return summonObjectDataList[id];
+    }
+    
+    public int GetSummonObjectDataCount()
+    {
+        return summonObjectDataList.Count;
+    }
+    
+    public int GetRandomId()
+    {
+        return UnityEngine.Random.Range(0, summonObjectDataList.Count);
+    }
+    
+}
