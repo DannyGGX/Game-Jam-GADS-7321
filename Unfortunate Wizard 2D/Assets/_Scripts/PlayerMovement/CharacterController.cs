9adeac7a2ecfb06970cf67e623bc6a3c243409d2
Jumpman777 <ST10062913@vgconnect.edu.za>
2024-04-14 14:25:08 +0200

    Animator Controller
    Added animations and player controller

--- Unfortunate Wizard 2D/Assets/_Scripts/PlayerMovement/CharacterController.cs
+++ Unfortunate Wizard 2D/Assets/_Scripts/PlayerMovement/CharacterController.cs
@@ -4,47 +4,88 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    
-    public Rigidbody2D rb2D;
+    public Rigidbody2D rb;
+    public Animator animator;
+    public SpriteRenderer player;
     public float jumpForce;
-    public float playerSpeed;
-    public Vector2 jumpHeight;
-    public bool isGrounded;
-    public float posRadius;
+    public float moveSpeed;
 
-    public LayerMask ground;
-    public Transform playerPos;
+    private bool isJumping;
+    private bool canJump = true;
 
-
-
-    void Start()
+    private void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        player = GetComponent<SpriteRenderer>();
     }
 
-
-    void Update()
+    private void Update()
     {
-        if (Input.GetAxis("Horizontal") != 0)
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        if (moveHorizontal != 0f)
         {
-            if (Input.GetAxisRaw("Horizontal") > 0)
-            {
-               
-                rb2D.AddForce(Vector2.right * playerSpeed);
-            }
+            Move(moveHorizontal);
+        }
+        else
+        {
+            StopMoving();
+        }
 
-            if (Input.GetAxisRaw("Horizontal") < 0)
-            {
-                
-                rb2D.AddForce(Vector2.left * playerSpeed);
-            }
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping && canJump)
+        {
+            Jump();
+            StopMoving();
+        }
 
-            isGrounded = Physics2D.OverlapCircle(playerPos.position, posRadius, ground);
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            StopJump();
+        }
+    }
 
-            if (isGrounded == true && Input.GetKeyDown("space"))
-            {
-                rb2D.AddForce(Vector2.up * jumpHeight);
-            }
+    private void Move(float direction)
+    {
+        rb.velocity = new Vector2(direction * moveSpeed, rb.velocity.y);
+        animator.SetBool("run", true);
+        if (direction < 0)
+        {
+            player.flipX = true;
+        }
+        else if (direction > 0)
+        {
+            player.flipX = false;
+        }
+    }
+
+    private void StopMoving()
+    {
+        rb.velocity = new Vector2(0f, rb.velocity.y);
+        animator.SetBool("run", false);
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(new Vector2(rb.velocity.x, jumpForce));
+        animator.SetBool("jump", true);
+        isJumping = true;
+        canJump = false; // Disable jumping until the player lands
+    }
+
+    private void StopJump()
+    {
+        if (isJumping)
+        {
+            isJumping = false;
+            animator.SetBool("jump", false);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("Object"))
+        {
+            canJump = true; // Enable jumping when the player lands on the ground or the object with the desired tag
         }
     }
 }
