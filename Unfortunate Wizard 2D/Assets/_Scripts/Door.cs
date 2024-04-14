63d0b9c9680d957ae914e2ab850414e7589ff5dd
DannyGGX <danielglanz111@gmail.com>
2024-04-14 16:41:09 +0200

    Added code for the door

--- Unfortunate Wizard 2D/Assets/_Scripts/Door.cs
+++ Unfortunate Wizard 2D/Assets/_Scripts/Door.cs
@@ -1,6 +1,14 @@
+using System;
 using UnityEngine;
 
 public class Door : MonoBehaviour
 {
     
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            EventManager.onFinishLevel.Invoke();
+        }
+    }
 }
