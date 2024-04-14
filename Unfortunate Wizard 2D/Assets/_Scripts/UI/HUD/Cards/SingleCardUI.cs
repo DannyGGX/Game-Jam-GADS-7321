20ab253770f0a63929e68d19e6b4d408636cdcb3
DannyGGX <danielglanz111@gmail.com>
2024-04-14 18:58:51 +0200

    Added click logic to the cards and imported text mesh pro
    Also added card selection UI code

--- Unfortunate Wizard 2D/Assets/_Scripts/UI/HUD/Cards/SingleCardUI.cs
+++ Unfortunate Wizard 2D/Assets/_Scripts/UI/HUD/Cards/SingleCardUI.cs
@@ -1,14 +1,41 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(Button))]
 public class SingleCardUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI cardName;
     [SerializeField] private SummonObjectsDataSO summonObjectsData;
+    [SerializeField, Tooltip("In child object")] private CardSelectionUI cardSelectionUI;
+    private bool _isSelected;
+
+    private CardInfo _currentCard;
+
+    private void Awake()
+    {
+        GetComponent<Button>().onClick.AddListener(OnCardClicked);
+    }
+    
+    private void OnCardClicked()
+    {
+        if (_isSelected)
+        {
+            EventManager.onCardDeselected.Invoke();
+            _isSelected = false;
+        }
+        else
+        {
+            EventManager.onCardSelected.Invoke(_currentCard);
+            _isSelected = true;
+        }
+        cardSelectionUI.ChangeCardSelectionState(_isSelected);
+    }
     
     
     public void SetCardUI(CardInfo cardInfo)
     {
+        _currentCard = cardInfo;
         SummonObjectData data = summonObjectsData.GetSummonObjectData(cardInfo.summonObjectId);
         
         cardName.text = data.name.ReplaceUnderscoreWithSpace();
